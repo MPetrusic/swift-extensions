f30a643aeade70f8c70bccbd179efe8dcb757ad9
Milos Petrusic <petrusicmilos94@gmail.com>
2021-03-21 16:46:37 +0100

    Initial Commit

---
+++ Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Milos Petrusic on 21.3.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
